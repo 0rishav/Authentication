cb03d725088e2eb91b96268cb0d023da19a222ab
0rishav <rishavrajraj1155@gmail.com>
2024-10-24 21:54:12 +0530

    code added

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get("/",(req,res)=>{
   })
 })
 
+app.get("/test",(req,res)=>{
+  res.status(200).json({
+      success:true,
+      message:"API WORKING"
+  })
+})
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"))
