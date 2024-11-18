d29e8e5d17c0280b4b6112ef4ef77e2f67b31443
Rishav Raj <96721375+0rishav@users.noreply.github.com>
2024-11-18 13:50:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/test", (req, res) => {
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://127.0.0.1:5500",
+  origin: "https://codexuslabs.com",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
